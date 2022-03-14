7821ed58295a2ed8ac23e358743efdf04a7ed392
Tabish Rashidi <tabish.rashidi@gmail.com>
2022-03-14 16:48:14 -0400

    tabish1: Added in the code to parse in front-end, needs cleaning

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,21 +3,66 @@ import './App.css';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import React, { useState } from 'react';
+import mammoth from "mammoth/mammoth.browser";
 
 
 
 function App() {
 
-  const [file, setSelectedFile] = useState("");  
+  const [file, setSelectedFile] = useState("");
+  
+  const [title, setTitle] = useState("");
+  const [wholeText, setText] = useState("");
  
-  function getSelectedFile(eventFileBlob) {
-    setSelectedFile(eventFileBlob); 
-    console.log(file);
+//   function getSelectedFile(eventFileBlob) {
+//     console.log(eventFileBlob);
 
 
-    var reader = new FileReader();
+//     var reader = new FileReader();
+//     reader.readAsArrayBuffer(eventFileBlob);
+
+//     reader.onload = (e) => {
+//         console.log(e.target.result);
+//         var enc = new TextDecoder("utf-8");
+
+//         console.log(enc.decode(e.target.result));
     
-  }
+//         var mam = mammoth.extractRawText(e.target.result);
+
+//         console.log(mam); 
+//     }
+//   }
+
+    // reader.onload = () => {
+    //     extractRawText({ eventFileBlob })
+    //         .then(result => {console.log(result.value)})
+    // }}
+
+
+  function extractWordRawText(arrayBuffer) {
+    mammoth
+      .extractRawText({ arrayBuffer })
+      .then(result => {
+        const text = result.value; // The raw text
+        const messages = result.messages; // Please handle messages
+        setText( text );
+
+        console.log(text); 
+      })
+      .done();
+
+
+  };
+
+  function handleFileChange(file) {
+    const reader = new FileReader();
+    reader.readAsArrayBuffer(file);
+    reader.onload = e => {
+      extractWordRawText(e.target.result);
+    };
+
+    setTitle( file.name );
+  };
 
 
   return (
@@ -32,7 +77,7 @@ function App() {
 
 
       <Button variant="contained" component="label"> Upload File
-        <input type="file" onChange={(e) => getSelectedFile(e.target.files[0])} hidden/>
+        <input type="file" onChange={(e) => handleFileChange(e.target.files[0])} hidden/>
       </Button>
 
       </header>
