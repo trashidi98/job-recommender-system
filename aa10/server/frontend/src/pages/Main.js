f15fe7dd26f2fde97d2d1dfd4489fa8a645229d6
trashidi98 <tabish.rashidi@gmail.com>
2022-03-24 23:38:14 -0400

    Adding in new background and animations

--- aa10/server/frontend/src/pages/Main.js
+++ aa10/server/frontend/src/pages/Main.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import {Routes, Route} from 'react-router-dom';
-import Axios from "axios";
 
 import HomePage from './HomePage';
 import ListJobs from './ListJobs';
+import LoadingScreen from'./LoadingScreen'; 
 
 const Main = () => {
   return (
     <Routes> {/* The Routes decides which component to show based on the current URL.*/}
       <Route exact path='/' element={<HomePage/>}></Route>
+      <Route exact path='/Loading' element={<LoadingScreen/>}></Route>
       <Route exact path='/ListJobs' element={<ListJobs/>}></Route>
     </Routes>
   );
